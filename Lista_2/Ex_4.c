26dfd4a336d19c1645db02079aa1e7990c620221
Afros-brasiliensis <athoskolling@gmail.com>
2025-03-26 13:15:16 -0300

    Update Ex_4.c

--- Lista_2/Ex_4.c
+++ Lista_2/Ex_4.c
@@ -2,5 +2,19 @@
 
 int main()
     {
-  printf("Quanto custa o computador? ");
+  float  nota_A, nota_B, peso_A, peso_B, media_final;
+    printf("Digite a nota do grau A: ");
+    scanf("%f", &nota_A);
+    printf("Digite a nota do grau B: ");
+    scanf("%f", &nota_B);
+
+    peso_A = 1.0/3.0;
+    peso_B = 2.0/3.0;
+
+    media_final = (nota_A * peso_A) + (nota_B * peso_B);
+
+    printf("A media final e %.1f", media_final);
+
+    return 0;
+
   }
