2cd6dc1ca0181514dc6e35a8d538d4bd969c96d6
Afros-brasiliensis <athoskolling@gmail.com>
2025-03-26 12:50:20 -0300

    Update Ex_3.c

--- Lista_2/Ex_3.c
+++ Lista_2/Ex_3.c
@@ -1,3 +1,17 @@
-//
-// Created by davic on 26/03/2025.
-//
+#include <stdio.h>
+
+int main()
+    {
+  float peso_prato, preco_quilo, valor_prato;
+
+  preco_quilo = 40.00;
+
+  printf("Qual peso do prato em Kg? ");
+  scanf("%f", &peso_prato);
+
+  valor_prato = peso_prato * preco_quilo;
+
+  printf("O valor total do prato e R$%.2f", valor_prato);
+
+  return 0;
+  }
