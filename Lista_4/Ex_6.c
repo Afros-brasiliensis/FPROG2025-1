335b661788445b212507946558aeb276e6931f05
Afros-brasiliensis <athoskolling@gmail.com>
2025-04-29 19:25:25 -0300

    final commmit

--- Lista_4/Ex_6.c
+++ Lista_4/Ex_6.c
@@ -2,3 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
+
+int main() {
+  int i, num, maior = -1, menor = 101, soma = 0;
+  float media;
+
+  srand(time(NULL));
+
+  for (i=0; i<5; i++){
+
+    num = rand() % 100 + 1;
+    printf("%d\n", num);
+
+    if (num > maior) {
+      maior = num;
+    }
+    if (num < menor) {
+      menor = num;
+    }
+    soma += num;
+  }
+  printf("O maior numero sorteado eh %d\n", maior);
+  printf("O menor numero sorteado eh %d\n", menor);
+  media = soma / 5;
+  printf("A media dos numeros sorteados eh %.2f\n", media);
+  printf("A soma dos numeros sorteados eh %d\n", soma);
+  return 20;
+}
--- Lista_4/Ex_8.c
+++ Lista_4/Ex_8.c
@@ -2,3 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
+
+int main(){
+
+	int num, fatorial;
+        char continuar;
+
+    do{
+		fatorial = 1;
+       printf("Digite um numero para calcular um fatorial:\n");
+        scanf("%d", &num);
+
+    	for(int i = 1; i <= num; i++){
+          fatorial *= i;
+    		printf("%d\n", fatorial);
+          }
+    	printf("Deseja continuar e calcular outro fatorial? (s/n)\n");
+    	scanf(" %c", &continuar);
+
+    	continuar = toupper(continuar);
+
+    	}while (continuar == 'S');
+
+       return 0;
+       }
